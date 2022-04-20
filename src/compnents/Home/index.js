4269858058124adfd22c2a330d36a5fd29162068
Krishan Kumar Dhabahi <96042801+wrestler93@users.noreply.github.com>
2022-04-20 10:11:34 +0530

    Add files via upload

---
+++ src/compnents/Home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import App from "./App";
+
+function index() {
+  return (
+    <div className='container text-center'>
+     
+      <App />
+    </div>
+  );
+}
+
+export default index;
